6feceb3fb0f0c2c7f668b54e7a2f4106a548a57c
hacknus <l_stoeckli@bluewin.ch>
2021-02-14 11:55:53 +0100

    implemented RSA (only works if encrypted letters are integers between 1000 and 9999, this depends on encryption key chosen)

--- piquiet/echo-client.py
+++ piquiet/echo-client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+from piquiet.rsa import encrypt, decrypt
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
@@ -9,9 +10,18 @@ PORT = 65432        # The port used by the server
 def listen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-    return repr(data)
+        message = "hello world"
+        enc = encrypt(message)
+        t = ""
+        for e in enc:
+            t += str(e)
+        enc = int(t)
+        s.sendall(int.to_bytes(enc, 32, "big"))
+        data = int.from_bytes(s.recv(1024),"big")
+        data = str(data)
+        d = [int(data[i:i + 4]) for i in range(0, len(data), 4)]
+
+    return decrypt(d)
 
 
 if __name__ == "__main__":
