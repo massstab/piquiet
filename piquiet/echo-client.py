df1667525fac44e3214becf353c64c1ee6a0864e
hacknus <l_stoeckli@bluewin.ch>
2021-02-15 19:57:32 +0100

    implemented AES encryption

--- piquiet/echo-client.py
+++ piquiet/echo-client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import socket
+from piquiet.encryption import encrypt, decrypt, get_key
 
 
 def get_config(server, path="connection.config"):
@@ -10,11 +11,25 @@ def get_config(server, path="connection.config"):
 
 
 def listen(hostname, port):
+    key = get_key()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((hostname, port))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-    return repr(data)
+        message = "hello dave you little man"
+        enc, tag, nonce = encrypt(key, message.encode())
+        s.sendall(enc)
+        s.sendall(b"\n")
+        s.sendall(tag)
+        s.sendall(b"\n")
+        s.sendall(nonce)
+        s.sendall(b"\n")
+        print(enc, tag, nonce)
+        o = s.recv(1024)
+        print(enc)
+        o += s.recv(1024)
+        print(tag)
+        enc, tag, nonce, _ = o.split(b'\n')
+        data = decrypt(key, enc, tag, nonce)
+    return data
 
 
 if __name__ == "__main__":
