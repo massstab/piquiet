c9f04491ba5ba582fd6fa2e9344dd653327147c9
hacknus <l_stoeckli@bluewin.ch>
2021-02-16 01:34:31 +0100

    added docstring

--- piquiet/encryption.py
+++ piquiet/encryption.py
@@ -6,6 +6,14 @@ from PIL import Image
 
 
 def generate_key_from_image(path="sample_image.jpg"):
+    """
+    Generate a key from an image and store it in the .keys/AES.key file
+
+    :param path: image path
+    :type path: String
+    :return: key
+    :rtype: bytearray
+    """
     im = Image.open(path)
     shape = np.array(im).shape
     ding = int(np.product(np.array(im).reshape(np.product(shape)).shape))
@@ -13,31 +21,72 @@ def generate_key_from_image(path="sample_image.jpg"):
     d = {"key": str(k)}
     with open('.keys/AES.key', 'w') as f:
         json.dump(d, f)
-    return k
+    print(k)
+    return bytearray.fromhex(k)
 
 
 def generate_key_from_string():
+    """
+    Generate a key from an image and store it in the .keys/AES.key file
+
+    :return: key
+    :rtype: bytearray
+    """
     s = input("type something random:")
     k = hashlib.sha256(s.encode()).hexdigest()
     d = {"key": str(k)}
     with open('.keys/AES.key', 'w') as f:
         json.dump(d, f)
-    return k
+    print(k)
+    return bytearray.fromhex(k)
 
 
 def get_key(path=".keys/AES.key"):
+    """
+    Load key from .keys/AES.key file
+
+    :param path: file path
+    :type path: String
+    :return: key
+    :rtype: bytearray
+    """
     with open(path, 'r') as f:
         k = json.load(f)
     return bytearray.fromhex(k["key"])
 
 
 def encrypt(k, data):
+    """
+    Encrypt data with the given key
+
+    :param k: key
+    :type k: bytearray
+    :param data: data
+    :type data: bytearray
+    :return: ciphertext, tag and nonce
+    :rtype: bytearray, bytearray, bytearray
+    """
     cipher = AES.new(k, AES.MODE_EAX)
     ciphertext, t = cipher.encrypt_and_digest(data)
     return ciphertext, t, cipher.nonce
 
 
 def decrypt(k, ciphertext, t, n):
+    """
+    Decrypt the ciphertext using the key, tag and nonce. Validate using the tag.
+    Return False if invalid.
+
+    :param k: key
+    :type k: bytearray
+    :param ciphertext: encrypted data
+    :type ciphertext: bytearray
+    :param t: tag
+    :type t: bytearray
+    :param n: nonce
+    :type n: bytearray
+    :return: message or False
+    :rtype: String or Boolean
+    """
     cipher = AES.new(k, AES.MODE_EAX, nonce=n)
     data = cipher.decrypt(ciphertext)
     try:
@@ -48,20 +97,17 @@ def decrypt(k, ciphertext, t, n):
 
 
 if __name__ == "__main__":
-    message = b"hello world"
+    # key = generate_key_from_string()      # uncomment to generate key
 
-    # key = generate_key_from_string()
-    #
-    # print(key)
-    #
-    # key = bytearray.fromhex(key)
-    #
-    # print(key)
-
+    # load the key
     key = get_key()
 
+    message = b"hello world"
+
+    # encrypt the message
     enc, tag, nonce = encrypt(key, message)
 
+    # decrypt the message
     text = decrypt(key, enc, tag, nonce)
 
     print(f"{message} >> {enc} >> {text}")
