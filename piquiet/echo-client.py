038cc8c8dc829ca360b124b22cbe03dbd7b7eba6
hacknus <l_stoeckli@bluewin.ch>
2021-02-16 01:12:39 +0100

    cleaned up implementation of encryption

--- piquiet/echo-client.py
+++ piquiet/echo-client.py
@@ -4,33 +4,49 @@ import socket
 from piquiet.encryption import encrypt, decrypt, get_key
 
 
-def get_config(server, path="connection.config"):
-    with open(path, 'r') as f:
-        c = json.load(f)
-    return c[server]["hostname"], c[server]["port"]
+class Server:
 
+    def __init__(self, server):
+        self.hostname, self.port = self.get_config(server)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def listen(hostname, port):
-    key = get_key()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((hostname, port))
-        message = "hello dave you little man"
+    @staticmethod
+    def get_config(server, path="connection.config"):
+        with open(path, 'r') as f:
+            c = json.load(f)
+        return c[server]["hostname"], c[server]["port"]
+
+    def send(self, message):
+        key = get_key()
         enc, tag, nonce = encrypt(key, message.encode())
-        s.sendall(enc)
-        s.sendall(b"\n")
-        s.sendall(tag)
-        s.sendall(b"\n")
-        s.sendall(nonce)
-        s.sendall(b"\n")
-        print(enc, tag, nonce)
-        o = s.recv(1024)
-        print(enc)
-        o += s.recv(1024)
-        print(tag)
-        enc, tag, nonce, _ = o.split(b'\n')
+        self.s.connect((self.hostname, self.port))
+        self.s.sendall(enc)
+        self.s.sendall(b"\t")
+        self.s.sendall(tag)
+        self.s.sendall(b"\t")
+        self.s.sendall(nonce)
+        self.s.sendall(b"\n")
+
+    def listen(self):
+        key = get_key()
+        o = b""
+        while True:
+            data = self.s.recv(1024)
+            if not data:
+                break
+            o += data
+            if b"\n" in data:
+                break
+        o = o.replace(b"\n", b"")
+        print(o)
+        enc, tag, nonce = o.split(b'\t')
         data = decrypt(key, enc, tag, nonce)
-    return data
+        return data
 
 
 if __name__ == "__main__":
-    print(f'Received: {listen(*get_config("dave"))}')
+    TCP = Server("dave")
+    # TCP.send("hi dave, this is a super long message. ueble sache maloney..
+    # cool stuff.. yes.. und denn ischsi verruuckt worde.. heute hast du aber hunger, mann")
+    TCP.send("hi dave, this is a short message")
+    print(TCP.listen())
